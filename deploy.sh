12d4bf8a65e48fc1cbefc2066619872dc5505d8b
Mike Yuan <me@yhndnzj.com>
2021-07-30 02:11:17 +0800

    Notify about README.md

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -e
+req="requirements.txt"
+readme="README.md"
 cd "$(dirname "$0")"
 
 for pkg in "$@"
 do
     stow --restow "$pkg"
-    req="$pkg/requirements.txt"
+    cd "$pkg"
     if [[ -f $req ]]; then
         echo -e "\nRequirements for $pkg:"
         cat "$req"
+        echo
     fi
+    if [[ -f $readme ]]; then
+        echo -e "\nPlease refer to $pkg/$readme for more information.\n"
+    fi 
+    cd ..
 done
