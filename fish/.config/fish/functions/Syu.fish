6250b5c008e195fa20caeeab8b82f4e0356c3606
Mike Yuan <me@yhndnzj.com>
2021-08-26 03:11:34 +0800

    Switch to paru for fish

--- fish/.config/fish/functions/Syu.fish
+++ fish/.config/fish/functions/Syu.fish
@@ -1,6 +1,6 @@
 # Defined in /home/yhndnzj/.config/fish/functions/Syu.fish @ line 2
 function Syu --description 'Upgrade the system'
-    yay -Syu --devel
-    yay -Yc
-    yay --aur -Sc --noconfirm >/dev/null
+    paru -Syu --devel
+    paru -c
+    paru --aur -Sc --noconfirm >/dev/null
 end
