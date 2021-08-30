5b2491bf493b537cad845b0274aff0d72e718892
Mike Yuan <me@yhndnzj.com>
2021-08-31 00:09:58 +0800

    Paru cleanup only after a successful Syu

--- fish/.config/fish/functions/Syu.fish
+++ fish/.config/fish/functions/Syu.fish
@@ -1,6 +1,8 @@
 # Defined in /home/yhndnzj/.config/fish/functions/Syu.fish @ line 2
 function Syu --description 'Upgrade the system'
     paru -Syu --devel
-    paru -c
-    paru --aur -Sc --noconfirm >/dev/null
+    if [ $status == 0 ]
+        paru -c
+        paru --aur -Sc --noconfirm >/dev/null
+    end
 end
