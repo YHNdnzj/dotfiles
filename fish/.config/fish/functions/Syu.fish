3cf58f16657bdffac7d36ebd7a5753acf30ce2f6
Mike Yuan <me@yhndnzj.com>
2021-08-31 00:19:37 +0800

    Fix Syu status test
    (cherry picked from commit a7b2b755efd8b1a6862946228dca59874257d78d)

--- fish/.config/fish/functions/Syu.fish
+++ fish/.config/fish/functions/Syu.fish
@@ -1,7 +1,7 @@
 # Defined in /home/yhndnzj/.config/fish/functions/Syu.fish @ line 2
 function Syu --description 'Upgrade the system'
     paru -Syu --devel
-    if [ $status == 0 ]
+    if test $status -eq 0
         paru -c
         paru --aur -Sc --noconfirm >/dev/null
     end
