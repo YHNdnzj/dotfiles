aaad2521b16b4450dc16cd4d99b0d8f309c9dc94
Mike Yuan <me@yhndnzj.com>
2021-08-31 20:52:32 +0800

    ncmpcpp: load module-native-protocol-tcp automatically

--- ncmpcpp/.config/fish/functions/ncmpcpp.fish
+++ ncmpcpp/.config/fish/functions/ncmpcpp.fish
@@ -1,4 +1,5 @@
 # Defined in /home/yhndnzj/.config/fish/functions/ncmpcpp.fish @ line 2
-function ncmpcpp --wraps ncmpcpp --description 'ncmpcpp with proxy'
+function ncmpcpp --wraps ncmpcpp --description 'Load module-native-protocol-tcp & Run ncmpcpp with proxy'
+    pactl load-module module-native-protocol-tcp
     px ncmpcpp
 end
