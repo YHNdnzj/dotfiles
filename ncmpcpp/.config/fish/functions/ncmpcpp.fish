fb9b9e4f6967b8b4318a49d41d2b8a76b9990d2e
Mike Yuan <me@yhndnzj.com>
2021-08-31 21:06:18 +0800

    ncmpcpp: load module only when not loaded currently

--- ncmpcpp/.config/fish/functions/ncmpcpp.fish
+++ ncmpcpp/.config/fish/functions/ncmpcpp.fish
@@ -1,5 +1,7 @@
 # Defined in /home/yhndnzj/.config/fish/functions/ncmpcpp.fish @ line 2
 function ncmpcpp --wraps ncmpcpp --description 'Load module-native-protocol-tcp & Run ncmpcpp with proxy'
-    pactl load-module module-native-protocol-tcp
+    if pactl list modules short | grep -q module-native-protocol-tcp
+        pactl load-module module-native-protocol-tcp
+    end
     px ncmpcpp
 end
