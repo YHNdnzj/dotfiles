78301ebd3662e9823a1d7d2adaf3062bc49e412f
Mike Yuan <me@yhndnzj.com>
2021-09-04 18:59:33 +0800

    ncmpcpp: hide pactl output & run only if pactl exits successfully

--- ncmpcpp/.config/fish/functions/ncmpcpp.fish
+++ ncmpcpp/.config/fish/functions/ncmpcpp.fish
@@ -1,7 +1,9 @@
 # Defined in /home/yhndnzj/.config/fish/functions/ncmpcpp.fish @ line 2
 function ncmpcpp --wraps ncmpcpp --description 'Load module-native-protocol-tcp & Run ncmpcpp with proxy'
     if not pactl list modules short | grep -q module-native-protocol-tcp
-        pactl load-module module-native-protocol-tcp
+        pactl load-module module-native-protocol-tcp >/dev/null
     end
-    px ncmpcpp
+    if test $status -eq 0
+        px ncmpcpp
+    end
 end
