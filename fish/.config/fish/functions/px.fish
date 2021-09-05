742faa54624cea47bf242f44e107483880d66078
Mike Yuan <me@yhndnzj.com>
2021-09-05 13:27:50 +0800

    px.fish: don't proxy .local

--- fish/.config/fish/functions/px.fish
+++ fish/.config/fish/functions/px.fish
@@ -1,5 +1,5 @@
 # Defined in /home/yhndnzj/.config/fish/functions/px.fish @ line 2
 function px --description 'Run command with envvars set for privoxy'
     set -l proxy http://127.0.0.1:8118
-    env http_proxy=$proxy https_proxy=$proxy ftp_proxy=$proxy rsync_proxy=$proxy no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com" $argv
+    env http_proxy=$proxy https_proxy=$proxy ftp_proxy=$proxy rsync_proxy=$proxy no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com,.local" $argv
 end
