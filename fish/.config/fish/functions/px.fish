22b9da75f639f147202fa93252094e45958cb643
Mike Yuan <me@yhndnzj.com>
2021-09-05 13:44:33 +0800

    px.fish: go back to use env

--- fish/.config/fish/functions/px.fish
+++ fish/.config/fish/functions/px.fish
@@ -1,5 +1,5 @@
 # Defined in /home/yhndnzj/.config/fish/functions/px.fish @ line 2
 function px --description 'Run command with envvars set for privoxy'
     set -l proxy http://127.0.0.1:8118
-    {http,https}_proxy=$proxy no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com,.local" $argv
+    env {http,https}_proxy=$proxy no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com,.local" $argv
 end
