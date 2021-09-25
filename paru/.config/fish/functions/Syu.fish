79deaaf29c9a21995df203c254554082e3950768
Mike Yuan <me@yhndnzj.com>
2021-09-25 13:59:22 +0800

    paru: Syu.fish: fix missing options

--- paru/.config/fish/functions/Syu.fish
+++ paru/.config/fish/functions/Syu.fish
@@ -1,6 +1,6 @@
 # Defined in /home/yhndnzj/.config/fish/functions/Syu.fish @ line 2
 function Syu --description 'Upgrade the system'
-    paru --repo
+    paru --repo -Syu
     px paru
     if test $status -eq 0
         paru -c
