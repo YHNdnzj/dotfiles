832587cb7e286d2c65d5fe214cd84bbc61d0035d
Mike Yuan <me@yhndnzj.com>
2021-09-25 14:05:56 +0800

    Revert "paru: Syu.fish: show news only on repopkg upgrade"
    This reverts commit 2571418dc0312787e13cb2f346177f8b399ac710.

--- paru/.config/fish/functions/Syu.fish
+++ paru/.config/fish/functions/Syu.fish
@@ -1,6 +1,6 @@
 # Defined in /home/yhndnzj/.config/fish/functions/Syu.fish @ line 2
 function Syu --description 'Upgrade the system'
-    paru --repo -Syu --news; and px paru --aur -Syu
+    paru --repo -Syu; and px paru --aur -Syu
     if test $status -eq 0
         paru -c
         paru --aur -Sc --noconfirm >/dev/null
