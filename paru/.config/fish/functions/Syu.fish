87ae890dcb619248913277ee758e527257711699
Mike Yuan <me@yhndnzj.com>
2021-09-25 13:55:36 +0800

    paru: add paru.conf & Syu.fish from fish

--- paru/.config/fish/functions/Syu.fish
+++ paru/.config/fish/functions/Syu.fish
@@ -1,6 +1,7 @@
 # Defined in /home/yhndnzj/.config/fish/functions/Syu.fish @ line 2
 function Syu --description 'Upgrade the system'
-    paru -Syu --devel
+    paru --repo
+    px paru
     if test $status -eq 0
         paru -c
         paru --aur -Sc --noconfirm >/dev/null
