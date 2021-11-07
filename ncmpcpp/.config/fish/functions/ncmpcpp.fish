a74a5a2d3d257e1f3d58b72d472601782436395b
Mike Yuan <me@yhndnzj.com>
2021-11-07 13:15:23 +0800

    ncmpcpp: use local mpd instead

--- ncmpcpp/.config/fish/functions/ncmpcpp.fish
+++ ncmpcpp/.config/fish/functions/ncmpcpp.fish
@@ -1,8 +1,6 @@
 # Defined in /home/yhndnzj/.config/fish/functions/ncmpcpp.fish @ line 2
-function ncmpcpp --wraps ncmpcpp --description 'Load module-native-protocol-tcp & Run ncmpcpp with proxy'
-    if not pactl list modules short | grep -q module-native-protocol-tcp
-        pactl load-module module-native-protocol-tcp >/dev/null
-    end
+function ncmpcpp --wraps ncmpcpp --description 'Start mpd & Run ncmpcpp with proxy'
+    systemctl --user start mpd.service
     if test $status -eq 0
         px ncmpcpp
     end
