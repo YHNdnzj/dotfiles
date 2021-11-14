1fd9eddfdc1462da5ef91a6fa3cbcf93f7a84cdd
Mike Yuan <me@yhndnzj.com>
2021-11-14 15:22:56 +0800

    ncmpcpp: start mpdris2 simultaneously

--- ncmpcpp/.config/fish/functions/ncmpcpp.fish
+++ ncmpcpp/.config/fish/functions/ncmpcpp.fish
@@ -1,7 +1,8 @@
 # Defined in /home/yhndnzj/.config/fish/functions/ncmpcpp.fish @ line 2
-function ncmpcpp --wraps ncmpcpp --description 'Start mpd & Run ncmpcpp with proxy'
+function ncmpcpp --wraps ncmpcpp --description 'Start mpd, mpdris2 & Run ncmpcpp with proxy'
     systemctl --user start mpd.service
     if test $status -eq 0
+        mpDris2 >/dev/null & disown
         px ncmpcpp
     end
 end
