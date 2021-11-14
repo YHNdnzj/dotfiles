37208926bfd7a08b323c751944f26369e315b563
Mike Yuan <me@yhndnzj.com>
2021-11-14 15:31:22 +0800

    ncmpcpp: start only one mpdris2 instance

--- ncmpcpp/.config/fish/functions/ncmpcpp.fish
+++ ncmpcpp/.config/fish/functions/ncmpcpp.fish
@@ -1,8 +1,10 @@
 # Defined in /home/yhndnzj/.config/fish/functions/ncmpcpp.fish @ line 2
 function ncmpcpp --wraps ncmpcpp --description 'Start mpd, mpdris2 & Run ncmpcpp with proxy'
     systemctl --user start mpd.service
-    if test $status -eq 0
-        mpDris2 >/dev/null & disown
+    and begin
+        if not pidof -q mpDris2
+            mpDris2 >/dev/null & disown
+        end
         px ncmpcpp
     end
 end
