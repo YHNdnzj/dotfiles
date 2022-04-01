6a9c0b4fe46f1856fd11edc1f3e1d97306ffab75
Mike Yuan <me@yhndnzj.com>
2022-04-01 12:17:37 +0800

    Add branch for dflybsd-vps

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+req="requirements.txt"
+readme="README.md"
+cd "$(dirname "$0")"
+
+for pkg in "$@"
+do
+    stow --restow "$pkg"
+    cd "$pkg"
+    if [[ -f $req ]]; then
+        echo -e "\nRequirements for $pkg:"
+        cat "$req"
+    fi
+    echo
+    if [[ -f $readme ]]; then
+        echo -e "Please refer to $pkg/$readme for more information.\n"
+    fi 
+    cd ..
+done
---
+++ fish/.config/fish/conf.d/term.fish
@@ -0,0 +1,3 @@
+if test "$TERM" = "alacritty"
+    TERM=xterm-256color exec fish
+end
