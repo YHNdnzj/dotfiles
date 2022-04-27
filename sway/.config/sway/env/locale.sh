82f29b3d602195acc6298ffed0adb68ae1f26463
Mike Yuan <me@yhndnzj.com>
2022-04-27 17:30:28 +0800

    sway: set some locales to zh_CN.UTF-8

--- sway/.config/sway/env/locale.sh
+++ sway/.config/sway/env/locale.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 export LANG=zh_TW.UTF-8
 export LANGUAGE=zh_TW:zh_CN:zh:en_US:en
+
+locale="zh_CN.UTF-8"
+LC_vars=(LC_ADDRESS LC_IDENTIFICATION LC_MONETARY LC_MEASUREMENT LC_NAME LC_NUMERIC LC_PAPER LC_TELEPHONE LC_TIME)
+for var in "${LC_vars[@]}"
+do
+    export "$var"="$locale"
+done
