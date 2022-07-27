2c205c3fd7b0a1a05bfa4d78b6a7bba412b91f38
Mike Yuan <me@yhndnzj.com>
2022-07-28 03:38:20 +0800

    fish: set px function to wrap env for completion

--- fish/.config/fish/functions/px.fish
+++ fish/.config/fish/functions/px.fish
@@ -1,5 +1,5 @@
 # Defined in /home/yhndnzj/.config/fish/functions/px.fish @ line 2
-function px --description 'Run command with envvars set for privoxy'
+function px --description 'Run command with envvars set for privoxy' --wraps env
     set -l proxy http://127.0.0.1:8118
     env {http,https}_proxy=$proxy no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com,.local" $argv
 end
