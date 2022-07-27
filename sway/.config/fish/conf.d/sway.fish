22e22216711e665a583d4e5bdaee93a16ee5a1e2
Mike Yuan <me@yhndnzj.com>
2022-07-28 01:21:02 +0800

    sway: add check for login shell before exec-ing

--- sway/.config/fish/conf.d/sway.fish
+++ sway/.config/fish/conf.d/sway.fish
@@ -1,3 +1,4 @@
-if test (tty) = "/dev/tty1" -a \( -z "$WAYLAND_DISPLAY" -a -z "$DISPLAY" \)
+if status is-login
+    and test (tty) = "/dev/tty1" -a \( -z "$WAYLAND_DISPLAY" -a -z "$DISPLAY" \)
     exec sway
 end
