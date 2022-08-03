45c32a256b3580f528da39cf5522686f90c22bbc
Mike Yuan <me@yhndnzj.com>
2022-08-04 02:02:28 +0800

    fish: Syu.fish: don't proceed when paru fails

--- fish/.config/fish/functions/Syu.fish
+++ fish/.config/fish/functions/Syu.fish
@@ -1,9 +1,7 @@
 # Defined in /home/yhndnzj/.config/fish/functions/Syu.fish @ line 2
 function Syu --description 'Upgrade the system'
     paru --repo -Syu; and px paru --aur -Syu
-    if test $status -eq 0
-        paru -c
-        paru --aur -Sc --noconfirm >/dev/null
-    end
-    px flatpak update
+    and paru -c
+    and paru --aur -Sc --noconfirm >/dev/null
+    and px flatpak update
 end
