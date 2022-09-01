84387e78c64a1645a8b4315bb5a3e2a0c75a048b
Mike Yuan <me@yhndnzj.com>
2022-09-01 09:42:51 +0800

    sync with master

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,4 +1,6 @@
 # Defined in /home/yhndnzj/.config/fish/functions/fish_greeting.fish @ line 2
 function fish_greeting
     echo Welcome to fish on (set_color blue)$hostname(set_color normal)!
+    echo It\'s now (set_color yellow; date +%T; set_color normal)
+    echo
 end
