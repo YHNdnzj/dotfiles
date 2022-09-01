42064f4753d1d6240f529240411ab0e37c0f2eaf
Mike Yuan <me@yhndnzj.com>
2022-09-01 21:57:53 +0800

    debuginfod: allow user-wide url config

--- debuginfod/.config/fish/conf.d/debuginfod.fish
+++ debuginfod/.config/fish/conf.d/debuginfod.fish
@@ -1,3 +1,8 @@
 if test -z $DEBUGINFOD_URLS
-    set -gx DEBUGINFOD_URLS (cat /etc/debuginfod/*.urls | string join ' ')
+    set -l DEBUGINFOD_DIRS /etc/debuginfod "$HOME/.config/debuginfod"
+    for dir in $DEBUGINFOD_DIRS
+        if test -d $dir
+            set -gx --append DEBUGINFOD_URLS (cat "$dir"/*.urls | string join ' ')
+        end
+    end
 end
