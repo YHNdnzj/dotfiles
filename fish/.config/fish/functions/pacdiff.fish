b714b08c90564078b17ffd9a8c1158a0a135e2eb
Mike Yuan <me@yhndnzj.com>
2022-09-07 11:20:29 +0800

    fish: use scripts from bat-extras

--- fish/.config/fish/functions/pacdiff.fish
+++ fish/.config/fish/functions/pacdiff.fish
@@ -1,4 +1,4 @@
 # Defined in /home/yhndnzj/.config/fish/functions/pacdiff.fish @ line 2
-function pacdiff --description "pacdiff with colored diff as DIFFPROG"
-    sudo env DIFFPROG="diff --color=always" pacdiff $argv
+function pacdiff --description "pacdiff with batdiff as DIFFPROG"
+    sudo DIFFPROG=batdiff pacdiff $argv
 end
