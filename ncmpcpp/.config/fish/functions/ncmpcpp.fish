361db4f073d2356e57ed6da7f5c73169bc5dbbc8
Mike Yuan <me@yhndnzj.com>
2022-09-10 15:29:07 +0800

    ncmpcpp: use systemd --user for mpDris2

--- ncmpcpp/.config/fish/functions/ncmpcpp.fish
+++ ncmpcpp/.config/fish/functions/ncmpcpp.fish
@@ -1,10 +1,5 @@
 # Defined in /home/yhndnzj/.config/fish/functions/ncmpcpp.fish @ line 2
 function ncmpcpp --description 'Start mpd, mpdris2 & Run ncmpcpp with proxy'
-    systemctl --user start mpd.service
-    and begin
-        if not pgrep -f mpDris2 &>/dev/null
-            mpDris2 &>/dev/null & disown
-        end
-        px ncmpcpp $argv
-    end
+    systemctl --user start mpd.service mpDris2.service
+    and px ncmpcpp $argv
 end
