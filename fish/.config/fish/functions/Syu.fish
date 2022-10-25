13721f99434d46db51a8697eb224db8106237696
Mike Yuan <me@yhndnzj.com>
2022-10-25 12:11:55 +0800

    fish: Syu.fish: use gfwlisted proxy for repo pkg updates

--- fish/.config/fish/functions/Syu.fish
+++ fish/.config/fish/functions/Syu.fish
@@ -1,7 +1,8 @@
 # Defined in /home/yhndnzj/.config/fish/functions/Syu.fish @ line 2
 function Syu --description 'Upgrade the system'
     set -l proxy http://127.0.0.1:8118
-    paru --repo -Syu; and px paru --aur -Syu --mflags "http_proxy=$proxy https_proxy=$proxy"
+    https_proxy=http://127.0.0.1:8119 paru --repo -Syu
+    and px paru --aur -Syu --mflags "http_proxy=$proxy https_proxy=$proxy"
     and paru -c
     and paru --aur -Sc --noconfirm >/dev/null
     and px flatpak update
