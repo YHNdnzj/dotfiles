6ce8206e6b3a3da1338677e482a2d7f78220ca94
Mike Yuan <me@yhndnzj.com>
2022-10-30 02:30:13 +0800

    fish: Syu.fish: let paru handle update ordering

--- fish/.config/fish/functions/Syu.fish
+++ fish/.config/fish/functions/Syu.fish
@@ -1,10 +1,14 @@
 # Defined in /home/yhndnzj/.config/fish/functions/Syu.fish @ line 2
 function Syu --description 'Upgrade the system'
     set -l proxy http://127.0.0.1:8118
-    https_proxy=http://127.0.0.1:8119 paru --repo -Syu
-    and px paru --aur -Syu --mflags "http_proxy=$proxy https_proxy=$proxy"
+    set -l acl_proxy http://127.0.0.1:8119
+
+    http_proxy=$acl_proxy https_proxy=$acl_proxy \
+        paru -Syu --mflags "http_proxy=$proxy https_proxy=$proxy"
+
     and paru -c
     and paru --aur -Sc --noconfirm >/dev/null
+
     and px flatpak update
     and flatpak uninstall --unused
 end
