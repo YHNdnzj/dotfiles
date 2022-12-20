ab88fcf66be33287ba776cddd50ba374cdafbbb5
Mike Yuan <me@yhndnzj.com>
2022-12-20 18:47:16 +0800

    ncmpcpp: switch to socket activation

--- ncmpcpp/.config/fish/functions/ncmpcpp.fish
+++ ncmpcpp/.config/fish/functions/ncmpcpp.fish
@@ -1,5 +1,5 @@
 # Defined in /home/yhndnzj/.config/fish/functions/ncmpcpp.fish @ line 2
-function ncmpcpp --description 'Start mpd, mpdris2 & Run ncmpcpp with proxy'
-    systemctl --user start mpd.service mpDris2.service
+function ncmpcpp --description 'Start mpdris2 & Run ncmpcpp with proxy'
+    systemctl --user start --no-block mpDris2.service
     and px ncmpcpp $argv
 end
