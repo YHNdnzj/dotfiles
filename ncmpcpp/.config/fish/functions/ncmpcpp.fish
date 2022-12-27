e70693e0819887f03d5b0559bee9429f8c60ae68
Mike Yuan <me@yhndnzj.com>
2022-12-27 22:28:02 +0800

    ncmpcpp: switch to mpd-mpris

--- ncmpcpp/.config/fish/functions/ncmpcpp.fish
+++ ncmpcpp/.config/fish/functions/ncmpcpp.fish
@@ -1,5 +1,5 @@
 # Defined in /home/yhndnzj/.config/fish/functions/ncmpcpp.fish @ line 2
 function ncmpcpp --description 'Start mpdris2 & Run ncmpcpp with proxy'
-    systemctl --user start --no-block mpDris2.service
+    systemctl --user start --no-block mpd-mpris.service
     and px ncmpcpp $argv
 end
