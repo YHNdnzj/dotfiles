785ebd2e489419509c959ca85734477629290b5e
Mike Yuan <me@yhndnzj.com>
2023-04-28 19:16:35 +0800

    mpv: update exit-fullscreen.lua

--- mpv/.config/mpv/scripts/exit-fullscreen.lua
+++ mpv/.config/mpv/scripts/exit-fullscreen.lua
@@ -1,15 +1,5 @@
--- https://github.com/zc62/mpv-scripts/blob/master/exit-fullscreen.lua
-
--- Exit fullscreen when playback ends, if keep-open=yes
-
-mp.observe_property("eof-reached", "bool", function(name, value)
-    if value then
-        local pause = mp.get_property_native("pause")
-        if pause then
-            local fullscreen = mp.get_property_native("fullscreen")
-            if fullscreen then
-                mp.set_property_native("fullscreen", false)
-            end
-        end
+mp.observe_property("eof-reached", "bool", function(_, eof)
+    if eof and mp.get_property_bool("pause") then
+        mp.set_property("fullscreen", "no")
     end
 end)
