e2d6e6ff8923778ada78025d5be8d32795dc57f9
Mike Yuan <me@yhndnzj.com>
2023-08-27 00:10:51 +0800

    Add generic branch 'arch-vps'

--- fish/.config/fish/functions/px.fish
+++
@@ -1,5 +0,0 @@
-# Defined in /home/yhndnzj/.config/fish/functions/px.fish @ line 2
-function px --description 'Run command with envvars set for http proxy' --wraps exec
-    set -l proxy http://127.0.0.1:8118
-    env {http,https}_proxy=$proxy no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com,.local" $argv
-end
--- paru/.config/fish/functions/Syu.fish
+++ paru/.config/fish/functions/Syu.fish
@@ -1,12 +1,6 @@
 # Defined in /home/yhndnzj/.config/fish/functions/Syu.fish @ line 2
 function Syu --description 'Upgrade the system'
-    set -l proxy http://127.0.0.1:8118
-    set -l acl_proxy http://127.0.0.1:8119
-
-    http_proxy=$acl_proxy https_proxy=$acl_proxy \
-        no_proxy=mirrors.bfsu.edu.cn,mirrors.ustc.edu.cn \
-        paru -Syu --mflags "http_proxy=$proxy https_proxy=$proxy"
-
+    paru
     and paru -c
     and paru --aur -Sc --noconfirm >/dev/null
 end
