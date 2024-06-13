fb71aab3e279f9cb3696d81050790d6962c1a867
Mike Yuan <me@yhndnzj.com>
2024-06-13 10:30:18 +0200

    ncmpcpp: replace fish function with wrapper script

--- ncmpcpp/dot-config/fish/functions/ncmpcpp.fish
+++
@@ -1,5 +0,0 @@
-# Defined in /home/yhndnzj/.config/fish/functions/ncmpcpp.fish @ line 2
-function ncmpcpp --description 'Start mpdris2 & Run ncmpcpp'
-    systemctl --user start --no-block mpd-mpris.service
-    and ncmpcpp $argv
-end
