9866bf12a8bf5fc218a9b72cc21a4f4b71beac37
Mike Yuan <me@yhndnzj.com>
2025-01-01 20:21:35 +0100

    deploy.sh: modernize a bit

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,28 @@
-#!/bin/bash -e
-req="requirements.txt"
-readme="README.md"
+#!/bin/bash
+set -eu
+
+declare -r req="requirements.txt" readme="README.md"
+
 cd "$(dirname "$0")"
 
-for pkg in "$@"
+_first=1
+for pkg in "${@%%/}"
 do
+    if (( ! _first )); then
+        echo "-------------------------------------------------------"
+    fi
+
     stow --restow "$pkg"
-    cd "$pkg"
+
+    pushd "$pkg" >/dev/null
     if [[ -f $req ]]; then
         echo -e "\nRequirements for $pkg:"
         cat "$req"
     fi
-    echo
     if [[ -f $readme ]]; then
-        echo -e "Please refer to $pkg/$readme for more information.\n"
+        echo -e "\nPlease refer to $pkg/$readme for more information."
     fi 
-    cd ..
+    popd >/dev/null
+
+    _first=0
 done
