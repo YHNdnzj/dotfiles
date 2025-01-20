22e8f307797eceff4d9abfe10353588cff32ee78
Mike Yuan <me@yhndnzj.com>
2025-01-20 21:46:23 +0100

    fish: search-and-replace: insist on second arg

--- fish/dot-config/fish/functions/search-and-replace.fish
+++ fish/dot-config/fish/functions/search-and-replace.fish
@@ -2,5 +2,8 @@
 function search-and-replace
     set -l files (rg --null --files-with-matches -- "$argv[1]" | sort -z | string split0)
     echo "Operating on: $files"
-    sed -i "s|$argv[1]|$argv[2]|g" $files
+
+    if test (count $argv) -gt 1
+        sed -i "s|$argv[1]|$argv[2]|g" $files
+    end
 end
