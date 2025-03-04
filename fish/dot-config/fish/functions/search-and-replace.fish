ccae39ae34f9919317f7036f6d8bea382830dc69
Mike Yuan <me@yhndnzj.com>
2025-03-04 20:08:12 +0100

    fish: search-and-replace: handle 0 matches properly

--- fish/dot-config/fish/functions/search-and-replace.fish
+++ fish/dot-config/fish/functions/search-and-replace.fish
@@ -1,6 +1,11 @@
 # Defined in /home/yhndnzj/.config/fish/functions/search-and-replace.fish @ line 2
 function search-and-replace
     set -l files (rg --null --files-with-matches -- "$argv[1]" | sort -z | string split0)
+    if test (count $files) -eq 0
+        echo "No file contains '$argv[1]'"
+        return 1
+    end
+
     echo "Operating on: $files"
 
     if test (count $argv) -gt 1
